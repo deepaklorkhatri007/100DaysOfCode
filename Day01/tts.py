d66e50042c44c7ed8119cbab4b53a5e999555b74
Deepak Khatri <robot@localhost.localdomain>
2018-08-17 21:53:28 +0530

    Text to Speech using python added

---
+++ Day01/tts.py
@@ -0,0 +1,11 @@
+# simple text to speech using Google TTS
+from gtts import gTTS
+import os
+import sys
+if len(sys.argv) == 2:
+    tts = gTTS(text=repr(sys.argv[1]), lang='en')
+    tts.save("text.mp3")
+    os.system("mpg123 text.mp3")
+    os.system("rm text.mp3")
+else :
+    print("Usage : python3 tts.py 'say this'")
