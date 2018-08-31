900bd40b86e83e6ca06db36bf69784db857abb0c
Deepak Khatri <deepaklorkhatri7@gmail.com>
2018-08-31 22:17:20 +0530

    some changes

--- Day01/tts.py
+++ Day01/tts.py
@@ -5,7 +5,8 @@ import sys
 if len(sys.argv) == 2:
     tts = gTTS(text=repr(sys.argv[1]), lang='en')
     tts.save("text.mp3")
-    os.system("mpg123 text.mp3")
+    for i in range (1,10):
+        os.system("mpg123 text.mp3")
     os.system("rm text.mp3")
 else :
     print("Usage : python3 tts.py 'say this'")
