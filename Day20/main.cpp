5dec7b2666f47e5c8904f1fd900f9e0704c7bd57
Deepak Khatri <deepaklorkhatri7@gmail.com>
2018-09-07 23:45:14 +0530

    cmake included

---
+++ Day20/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <sstream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+using namespace std;
+using namespace cv;
+int main(int argc, char const *argv[])
+{
+    
+    cout<<"working";
+    return 0;
+}
