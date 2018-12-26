4c4b16fdc7a67d873514107328149b6ff67a85c6
deepaklorkhatri007 <deepaklorkhatri7@gmail.com>
2018-12-26 23:51:50 +0530

    template decorator added

---
+++ Day60/tmplate_decorator.py
@@ -0,0 +1,12 @@
+from functools import wraps
+
+def decorator_name(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # 1. Code to execute BEFORE calling the decorated function
+
+        # 2. Call the decorated function as required, returing its
+        # resuls if needed.
+            return func(*args, **kwargs)
+        # 3. Code to execute INSTEAD of calling the decorated function
+    return wrapper
